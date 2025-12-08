6a9a274b4d9d491578e70c56f80c5d244c4dabe6
JaydenHuang555 <JaydenKanaHuang@outlook.com>
2025-12-08 15:04:23 -0800

    make modules public

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod unit;
+pub mod measure;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod geom;
+pub mod motion;
+pub mod time;
+pub mod num;
